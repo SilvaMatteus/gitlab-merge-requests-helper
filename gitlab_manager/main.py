bbe4f1cf71832add76b163e2cda7f5696546d37f
Matteus Silva <silvamatteus@lsd.ufcg.edu.br>
2020-03-02 11:41:12 -0300

    Verify when a mr is too old

--- gitlab_manager/main.py
+++ gitlab_manager/main.py
@@ -1,5 +1,7 @@
 import gitlab
-
+import datetime
+from dateutil import parser
+from dateutil.tz import gettz
 
 HEADER = '''
 What a beautiful day to review merge requests =)
@@ -7,15 +9,28 @@ What a beautiful day to review merge requests =)
 
 
 def show_mr(mr):
+    created_date = parser.isoparse(mr.created_at)
+    updated_date = parser.isoparse(mr.updated_at)
+    # Time elapsed warn!
+    today = datetime.datetime.now().replace(tzinfo=updated_date.tzinfo)
+    updated_delta = today - updated_date
+    created_delta = today - created_date
+
+    wip_text = '    #WIP' if mr.work_in_progress else ''
+    too_old_text = '    #TOO_OLD' if updated_delta > datetime.timedelta(
+        days=14) else ''
+    not_updated_text = '    #NOT_UPDATED' if updated_delta > datetime.timedelta(
+        days=7) else ''
+
     print('    ', mr.title)
     print('    ', mr.web_url)
     print('    ', 'Up votes:', mr.upvotes,
           ' ' * 8 + 'Down votes:', mr.downvotes)
     print('    ', 'MERGE STATUS: ', mr.merge_status, end='')
-    print('    ', '#WORK_IN_PROGRESS') if mr.work_in_progress else print()
+    print(wip_text + too_old_text + not_updated_text)
     print('    ', mr.source_branch, '-->', mr.target_branch)
-    print('    ', 'Created at:', mr.created_at,
-          'and updated at:', mr.updated_at)
+    print('    ', 'Created at:', created_date.strftime('%d-%b-%Y, %H:%M'),
+          'and updated at:', updated_date.strftime('%d-%b-%Y, %H:%M'))
     print('    ', 'By', mr.author['name'])
     print()
 
